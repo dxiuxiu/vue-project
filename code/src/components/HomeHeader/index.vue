e38bb6ad54e44b4ed2b8174c28084b75adec4cfe
duxiuxiu <duxx@summit.com.cn>
2020-04-07 15:26:02 +0800

    feat(fonts): 添加字体图标资源并测试

---
+++ code/src/components/HomeHeader/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home-header-container">
+        <i class="icon-angle-down"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHeader"
+};
+</script>
+
+<style scope>
+</style>
--- code/src/pages/Home/index.vue
+++ code/src/pages/Home/index.vue
@@ -1,12 +1,17 @@
 <template>
   <div class=''>
     home
+    <HomeHeader/>
   </div>
 </template>
 
 <script>
+import HomeHeader from '../../components/HomeHeader'
 export default {
   name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 

381c41a57188792467f77bd7eea2df9a195ced52
duxiuxiu <duxx@summit.com.cn>
2020-04-07 14:07:05 +0800

    feat(css): vue 做 css reset

--- code/src/main.js
+++ code/src/main.js
@@ -1,17 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
-
 import  routes from './router/index'
 import VueRouter from 'vue-router'
+
+import './assets/css/index.css' // css reset
 Vue.use(VueRouter);
 
 
 Vue.config.productionTip = false
 
+
 const router = new VueRouter({
     routes 
 })
+/** ?? */
 new Vue({
-  render: h => h(App),
+  render: h => h(App),  
   router
 }).$mount('#app')

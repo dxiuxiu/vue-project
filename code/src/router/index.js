c5a0edd1c35f8b21f80827eb345ecf04d2a46ffd
duxiuxiu <duxx@summit.com.cn>
2020-04-07 15:10:11 +0800

    chore(code): 根据项目需求做初始化准备

--- code/src/router/index.js
+++ code/src/router/index.js
@@ -1,39 +1,23 @@
 
-// import Page from '../pages/index.vue'
-// const Home = { template: '<div>home</div>' }
-const Test = { template: '<div>test</div>' }
-// import NotFound from '../components/NotFound/index.vue'
 
-const Page = () => import('../pages/index.vue')
 const Home = () => import('../pages/Home/index.vue')
 const User = () => import('../pages/User/index.vue')
 const NotFound = () => import('../components/NotFound/index.vue')
 // import App from '../App.vue'
 /** 默认值内容?? */
 const routes = [
-    {
-        path:'/',
-        component: Page,
-        redirect:'/home',
-        children: [
-            {
-              path: 'home',
-              component: Home
-            },
-            {
-              path: 'test',
-              component: Test
-            }
-          ]
-    },
-    {
-      path:'/user',
-      component:User
-    },
-    {
-      path:'/404',
-      component:NotFound
-    }
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/user',
+    component: User
+  },
+  {
+    path: '/404',
+    component: NotFound
+  }
 ]
 
 export default routes

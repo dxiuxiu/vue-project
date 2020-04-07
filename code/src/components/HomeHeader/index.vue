d8b5fdcfed2904d17412fcb6fe6510f3bbe5b7f6
duxiuxiu <duxx@summit.com.cn>
2020-04-07 15:51:13 +0800

    feat(HomeHeader): 新建首页头部组件

--- code/src/components/HomeHeader/index.vue
+++ code/src/components/HomeHeader/index.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home-header-container">
+    <div class="home-header-left">
+      <router-link to="/city">
+        <span>cityName</span>
+        &nbsp;
         <i class="icon-angle-down"></i>
+      </router-link>
+    </div>
+    <div class="home-header-middle">
+      <input placeholder='请输入关键字'/>
+    </div>
+    <div class="home-header-right">
+      <router-link to="/user">
+        <i class="icon-user"></i>
+      </router-link>
+    </div>
   </div>
 </template>
 
@@ -11,4 +25,32 @@ export default {
 </script>
 
 <style scope>
+.home-header-container {
+  display: flex;
+  padding: 10px;
+  background-color: red;
+}
+.home-header-left{
+  width: 100px;
+  line-height: 39px;
+}
+.home-header-middle{
+  flex: 1;
+  background-color: #fff;
+  border-radius: 5px;
+}
+input{
+  border: none;
+  padding: 10px;
+  font-size: 14px!important;
+}
+
+.home-header-right{
+  width: 30px;
+  line-height: 39px;
+}
+a,i{
+  color: #fff;
+}
+
 </style>

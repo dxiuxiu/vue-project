6c5769b729433526300bcb65dfa16792801e7fe6
duxiuxiu <duxx@summit.com.cn>
2020-04-07 16:56:06 +0800

    continue(HomeHeader): 新建首页头部组件

--- code/src/components/HomeHeader/index.vue
+++ code/src/components/HomeHeader/index.vue
@@ -1,19 +1,20 @@
 <template>
-  <div class="home-header-container">
-    <div class="home-header-left">
+  <div class="home-header-container clear-fix">
+    <div class="home-header-left float-left">
       <router-link to="/city">
         <span>cityName</span>
         &nbsp;
         <i class="icon-angle-down"></i>
       </router-link>
     </div>
-    <div class="home-header-middle">
-      <input placeholder='请输入关键字'/>
-    </div>
-    <div class="home-header-right">
+    <div class="home-header-right float-right">
       <router-link to="/user">
         <i class="icon-user"></i>
       </router-link>
+    </div>
+
+    <div class="home-header-middle">
+      <input placeholder="请输入关键字" />
     </div>
   </div>
 </template>
@@ -26,31 +27,42 @@ export default {
 
 <style scope>
 .home-header-container {
-  display: flex;
   padding: 10px;
   background-color: red;
 }
-.home-header-left{
+.home-header-left {
   width: 100px;
   line-height: 39px;
 }
-.home-header-middle{
-  flex: 1;
+.home-header-middle {
+  margin: 0px 40px 0px 100px;
   background-color: #fff;
   border-radius: 5px;
 }
-input{
+input {
   border: none;
   padding: 10px;
-  font-size: 14px!important;
+  font-size: 14px !important;
 }
 
-.home-header-right{
+.home-header-right {
   width: 30px;
   line-height: 39px;
 }
-a,i{
+a,
+i {
   color: #fff;
 }
 
+.float-left {
+  float: left;
+}
+.float-right {
+  float: right;
+}
+.clear-fix:after {
+  display: table;
+  content: "";
+  clear: both;
+}
 </style>

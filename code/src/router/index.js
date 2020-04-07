a217db632779a4e993895cd312aefd729a6c7f0a
duxiuxiu <duxx@summit.com.cn>
2020-04-07 15:03:33 +0800

    continue(router): 路由懒加载

--- code/src/router/index.js
+++ code/src/router/index.js
@@ -1,15 +1,19 @@
 
-// import Home from '../pages/Home/index.vue'
-import Ap from '../pages/index.vue'
-const Home = { template: '<div>home</div>' }
+// import Page from '../pages/index.vue'
+// const Home = { template: '<div>home</div>' }
 const Test = { template: '<div>test</div>' }
-import NotFound from '../components/NotFound/index.vue'
+// import NotFound from '../components/NotFound/index.vue'
+
+const Page = () => import('../pages/index.vue')
+const Home = () => import('../pages/Home/index.vue')
+const User = () => import('../pages/User/index.vue')
+const NotFound = () => import('../components/NotFound/index.vue')
 // import App from '../App.vue'
 /** 默认值内容?? */
 const routes = [
     {
         path:'/',
-        component: Ap,
+        component: Page,
         redirect:'/home',
         children: [
             {
@@ -21,6 +25,10 @@ const routes = [
               component: Test
             }
           ]
+    },
+    {
+      path:'/user',
+      component:User
     },
     {
       path:'/404',

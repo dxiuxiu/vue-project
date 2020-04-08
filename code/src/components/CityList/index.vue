b698fc2e7ec1aaa87e9b6a9465df7826ad886856
duxiuxiu <duxx@summit.com.cn>
2020-04-08 16:48:00 +0800

    feat(CityList): 推荐城市列表展示

---
+++ code/src/components/CityList/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <ul class="city-list-container">
+    <li class="city-item" @click="clickHandle('上海')">上海</li>
+    <li class="city-item" @click="clickHandle('北京')">北京</li>
+    <li class="city-item" @click="clickHandle('杭州')">杭州</li>
+    <li class="city-item" @click="clickHandle('无锡')">无锡</li>
+
+    <li class="city-item" @click="clickHandle('广州')">广州</li>
+    <li class="city-item" @click="clickHandle('西安')">西安</li>
+    <li class="city-item" @click="clickHandle('江苏')">江苏</li>
+    <li class="city-item" @click="clickHandle('浙江')">浙江</li>
+
+    <li class="city-item" @click="clickHandle('湖南')">湖南</li>
+    <li class="city-item" @click="clickHandle('湖北')">湖北</li>
+    <li class="city-item" @click="clickHandle('江西')">江西</li>
+    <li class="city-item" @click="clickHandle('汉中')">汉中</li>
+
+    <li class="city-item" @click="clickHandle('榆林')">榆林</li>
+    <li class="city-item" @click="clickHandle('测试')">---</li>
+    <li class="city-item" @click="clickHandle('测试')">---</li>
+    <li class="city-item" @click="clickHandle('测试')">---</li>
+  </ul>
+</template>
+<script>
+export default {
+  name: "CityList",
+  props: {
+    cityChange: {
+      type: Function,
+      default:null
+    }
+  },
+  methods: {
+    /** 这里写箭头函数 this 是会报错滴 */
+    clickHandle: function (cityName) {
+      if (!this.cityChange) {
+        return;
+      }
+      this.cityChange(cityName);
+    }
+    // clickHandle: (cityName) => {
+    //   if (!this.cityChange) {
+    //     return;
+    //   }
+    //   this.cityChange(cityName);
+    // }
+  },
+  data() {
+    return {};
+  },
+  activated() {}
+};
+</script>
+
+<style scoped>
+/* css */
+.city-list-container {
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: repeat(4, 40px);
+  padding: 15px 10px;
+}
+.city-item {
+  list-style: none;
+  text-align: center;
+  vertical-align: middle;
+  border: 1px solid #f1f1f1;
+  line-height: 40px;
+}
+</style>
--- code/src/pages/City/index.vue
+++ code/src/pages/City/index.vue
@@ -2,19 +2,24 @@
   <div class="city-container">
     <Header title="选择城市" />
     <CurrentCity :cityName="userInfo.cityName" />
+    <CityList :cityChange="cityChange"/>
   </div>
 </template>
 <script>
 import Header from "../../components/Header";
 import CurrentCity from "../../components/CurentCity";
+import CityList from "../../components/CityList"
 export default {
   name: "City",
   components: {
     Header,
-    CurrentCity
+    CurrentCity,
+    CityList
   },
   methods: {
-    
+    cityChange:(data) => {
+      console.log(data)
+    }
   },
   data() {
     return {

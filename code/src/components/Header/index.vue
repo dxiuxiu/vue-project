e5e24ff7fc993cd4599b27afbb39d9d6c61d8257
duxiuxiu <duxx@summit.com.cn>
2020-04-08 12:03:52 +0800

    feat(Header): 添加通用 Header 组件

---
+++ code/src/components/Header/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="common-header">
+    <span class="back-icon" @click="handleClick">
+      <i class="icon-chevron-left"></i>
+    </span>
+
+    <h1>{{title}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "title"
+    }
+  },
+  methods: {
+    handleClick: () => {
+      console.log('handleClick')
+      window.history.back();
+      // router.go(-1)
+    }
+  },
+  data() {
+    return {
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* css */
+.common-header {
+  display: flex;
+  text-align: center;
+  padding: 15px 10px;
+  color: white;
+  background-color: red;
+}
+.back-icon {
+  width: 60px;
+  height: 16px;
+  cursor: pointer;
+  padding: 9px 0px;
+}
+h1 {
+  flex: 1;
+  font-size: 16px;
+  line-height: 1;
+}
+</style>
--- code/src/pages/City/index.vue
+++ code/src/pages/City/index.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="city-container">
+      <Header title="选择城市"/>
       city
   </div>
 </template>
 <script>
+import Header from '../../components/Header'
 export default {
-  name: 'City'
+  name: 'City',
+  components: {
+      Header,
+  }
 };
 </script>
 

eb701ce7d7df480ed9bc5716ce40e550a61a7b97
duxiuxiu <duxx@summit.com.cn>
2020-04-09 10:41:20 +0800

    comtinue(UserInfo): 用户页面的用户信息展示组件

---
+++ code/src/components/UserInfo/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="userInfo-container">
+    <p>
+      <i class="icon-user"></i>
+      &nbsp;
+      {{userInfo.userName}}
+    </p>
+    <p>
+      <i class="icon-map-marker"></i>
+      &nbsp;
+      {{userInfo.cityName}}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "UserInfo",
+  props: {
+    userInfo: {
+      type: Object,
+      require: true,
+      default: function() {
+        return {
+          userName: "default",
+          cityName: "default"
+        };
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* css */
+.userInfo-container {
+  background-color: #fff;
+  padding: 10px;
+}
+.userInfo-container p {
+  line-height: 1.5;
+  color: #666;
+  text-align: left;
+}
+</style>
--- code/src/pages/User/index.vue
+++ code/src/pages/User/index.vue
@@ -1,13 +1,29 @@
 <template>
-  <div class>User</div>
+  <div class>
+    <Header title="用户中心" />
+    <UserInfo :userInfo = "userInfo" />
+  </div>
 </template>
 
 <script>
-import router from '../../router'
+import Header from "../../components/Header";
+import UserInfo from "../../components/UserInfo";
+import router from "../../router";
 export default {
   name: "User",
+  components: {
+    Header,
+    UserInfo
+  },
+  data() {
+    return {
+      userInfo: this.$store.state.userInfo
+    };
+  },
   mounted() {
     const { userInfo } = this.$store.state;
+    this.userInfo = userInfo
+    console.log(this.userInfo)
     if (!userInfo.userName) {
       router.push(`/login`);
     }

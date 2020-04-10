38f470f948edb92dd09531b9c96c1739c5b8c101
duxiuxiu <duxx@summit.com.cn>
2020-04-10 09:42:22 +0800

    feat(Category): 目录组件的 tab 切换效果

--- code/src/pages/Home/index.vue
+++ code/src/pages/Home/index.vue
@@ -1,18 +1,21 @@
 <template>
   <div class=''>
     <HomeHeader/>
+    <Category/>
     <Discount/>
   </div>
 </template>
 
 <script>
 import HomeHeader from '../../components/HomeHeader'
+import Category from '../../components/Category'
 import Discount from './subPage/Discount'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    Discount
+    Discount,
+    Category
   }
 }
 </script>
